81d06e1e06eb99dd64d90c10306d1bc36b62bc94
kenedy <kipkiruikenedy@gmail.com>
2023-10-19 09:54:00 +0300

    change cors settings

--- src/main/java/com/cytek2/cytek/audit/configurations/CorsConfig.java
+++ src/main/java/com/cytek2/cytek/audit/configurations/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://44.208.22.32:3000/*");
+        config.addAllowedOrigin("http://44.208.22.32:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
